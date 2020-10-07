0c3d2588d7fe3f3c10f8df7e4c28b027f29428c8
dusekpe <dusekpe@seznam.cz>
2020-10-07 19:50:31 +0200

    lesson4

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
 
+   ongoinTraining = false;
+
   constructor() { }
 
   ngOnInit(): void {
